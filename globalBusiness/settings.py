5870888e79c14ae9c49f3b2c27c5dd07a5849135
GurleenKBhuller <gurleenkaurbhuller@gmail.com>
2023-10-28 18:12:52 -0700

    domain

--- globalBusiness/settings.py
+++ globalBusiness/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ur-uca7oagk#i^-fd&x&iku$3!+qrt-2qv1q(%hst)2@n8!=@w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '']
+ALLOWED_HOSTS = ['127.0.0.1', 'global-business-cbc1070b1aeb.herokuapp.com']
 
 
 # Application definition
