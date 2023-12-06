d30040ae306a34b93e126a832feb130ef15dfd45
GurleenKBhuller <gurleenkaurbhuller@gmail.com>
2023-12-06 14:19:37 -0800

    vercel

--- globalBusiness/urls.py
+++ globalBusiness/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 # from django.views.static import serve
 # from django.conf.urls.static import url
 urlpatterns = [
@@ -24,3 +26,5 @@ urlpatterns = [
 #     url(r'^media/(?P<path›.*)$', serve, {" document root": settings.MEDIA_ROOT}),
 # url(r'^static/(?P<path›.*)$', serve, {'document _root': settings.STATIC ROOT}),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
